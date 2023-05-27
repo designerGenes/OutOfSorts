4fe1ceb25aa28b97ed2525d07703ce40a255b253
designerjeans <jnationdesignerjeans@gmail.com>
2023-05-27 13:42:24 -0500

    add 05 26 and 27

--- 05_26_23.playground/Contents.swift
+++ 05_26_23.playground/Contents.swift
@@ -34,10 +34,47 @@ func bubbleSort<T: Comparable>(arr: inout [T]) -> [T] {
     return arr
 }
 
-func mergeSort<T: Comparable>(arr: inout [T]) -> [T] {
+func mergeSort<T: Comparable>(arr: [T]) -> [T] {
+    guard arr.count > 1 else {
+        return arr
+    }
+    
+    let midIdx = arr.count / 2
+    let leftArray = mergeSort(arr: Array(arr[0..<midIdx]))
+    let rightArray = mergeSort(arr: Array(arr[midIdx...]))
+    
+    
+    return merge(l: leftArray, r: rightArray)
+}
+
+func merge<T: Comparable>(l: [T], r: [T]) -> [T] {
+    var leftIdx = 0
+    var rightIdx = 0
+    var mergedArray: [T] = []
+    while leftIdx < l.count && rightIdx < r.count {
+        if l[leftIdx] < r[rightIdx] {
+            mergedArray.append(l[leftIdx])
+            leftIdx += 1
+        } else {
+            mergedArray.append(r[rightIdx])
+            rightIdx += 1
+        }
+    }
+    
+    while leftIdx < l.count {
+        mergedArray.append(l[leftIdx])
+        leftIdx += 1
+    }
+    
+    while rightIdx < r.count {
+        mergedArray.append(r[rightIdx])
+        rightIdx += 1
+    }
+    
+    return mergedArray
     
 }
 
 var myArr = Array(0..<100)
 var myScrambledArr = myArr.asScrambled()
-print(bubbleSort(arr: &myScrambledArr))
+print(mergeSort(arr: myScrambledArr))
