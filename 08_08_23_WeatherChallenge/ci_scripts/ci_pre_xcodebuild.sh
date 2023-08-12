0c0fb9e6a812a8623ebf8fcee0c05b7e9a8b9d55
designerjeans <jnationdesignerjeans@gmail.com>
2023-08-12 12:00:24 -0500

    add echo statement

--- 08_08_23_WeatherChallenge/ci_scripts/ci_pre_xcodebuild.sh
+++ 08_08_23_WeatherChallenge/ci_scripts/ci_pre_xcodebuild.sh
@@ -11,7 +11,7 @@ echo "Stage: PRE-Xcode Build is activated .... "
 # Move to the place where the scripts are located.
 # This is important because the position of the subsequently mentioned files depend of this origin.
 cd ..
-echo "PWD: $(PWD)"
+echo "OPENAIKEY: $OPENAI_API_KEY"
 plutil -replace OPENAI_API_KEY -string $OPENAI_API_KEY Info.plist
 plutil -p Info.plist
 
